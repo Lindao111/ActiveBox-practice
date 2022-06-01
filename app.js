2c9adff5b156d742b005b9087c54fa6ed2a863e4
Nikita <nekita321@gmail.com>
2022-06-01 21:52:39 +0300

    looooong dick

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 $(function(){
 
+
+    // fixed header
     let header = $("#header");
     let intro = $("#intro");
-    // let introH
+    let introH = intro.innerHeight();
     // let scrollPos = $(window).scrollTop();
 
     $(window).on("scroll load resize",function(){
-        introH = intro.innerHeight();
+        // introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
-        console.log(scrollPos);
         
+        // console.log(scrollPos);
+        
+        checkScroll(scrollPos, introH)
+    });
+    function checkScroll(scrollPos, introH){
         if(scrollPos > introH){
             header.addClass("fixed");
         } else{
             header.removeClass("fixed");
         }
-    });
+    };
 
     
-    header.addClass("fixed");
+    // smoothe scroll=============
+    $("[data-scroll]").on("click", function(event){
+        event.preventDefault();
+
+        let elementid = $(this).data("scroll");
+        let elementOffset = $(elementid).offset().top;
+
+        console.log(elementOffset);
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700 );
+
+    });
+
+    // Nav Toggle
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+        navToggle.on("click resize", function(event){
+        event.preventDefault();
+        
+        nav.toggleClass("show");
+    });
+
 
 });
